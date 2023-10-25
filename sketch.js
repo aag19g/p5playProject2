c26d5f1b78822a1050d7d576b81e35c33a9062cd
Alisha <144165734+aag19g@users.noreply.github.com>
2023-10-25 17:41:29 -0400

    Sprite and tiles created. Working on world next

--- sketch.js
+++ sketch.js
@@ -1,64 +1,73 @@
-// a simple game where you click to drop a ball and 
-// try to get as many balls as possible on the spinning shape
-// move the mouse up and down to change the rotation speed of the shape
-// use the right and left keys to make the shape larger or smaller
+let miniMe;
+let tiles, tilesGroup;
 
-let spinningShape;
-let highScore = 0;
-
-function setup() {
-    let canvas = new Canvas("fullscreen");
-
-    world.gravity.y = 10;
-
-    // this sprite can be created on a single line, but it's easier to read this way:
-	spinningShape = new Sprite();
-	spinningShape.width = canvas.width/5;
-	spinningShape.height = spinningShape.width;
-    spinningShape.collider = "kinematic";
-
-    textFont("Courier", 24);
+function preload(){
+    miniMe = new Sprite(15, 14, 15, 14);
+    miniMe.spriteSheet = 'miniMe.png';
+    miniMe.anis.frameDelay = 8
+    miniMe.addAnis({
+        front: {row: 0, frames: 4},
+        back: {row: 1, frames: 4},
+        right: {row: 2, frames: 4},
+        left: {row: 3, frames: 4},
+    });
+    miniMe.changeAni('front');
+    miniMe.collider = "k"
 }
 
-function draw() {
+function setup() {
+    new Canvas(500, 500, 'pixelated x4');
+    allSprites.pixelPerfect = true;
+    allSprites.rotationLock = true;
 
-    // try the game without this line :)
+    tiles = new Group();
+    tiles.w = 10;
+    tiles.h = 10;
+    tiles.tile = '=';
+    tiles.collider = 's';
+    tiles.color = '#5E8C61'
+
+    tilesGroup = new Tiles([
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        '================',
+        ],
+        50,
+        40,
+        tiles.w + 0,
+        tiles.h + 0)
+}
+
+function draw() { 
     clear();
-
-    // the map function translates a value from one range to another
-    // https://p5js.org/reference/#/p5/map
-    spinningShape.rotationSpeed = map(mouse.y, 0, canvas.height, -10, 10);
-
-    // create a ball when the mouse is clicked
-    // https://p5play.org/learn/input_devices.html
-    if (mouse.presses()) {
-        let ball = new Sprite(mouse.x, -20, 20);
-        // make the ball resist rolling when it touches the spinning shape
-        // https://p5play.org/learn/sprite.html?page=9
-        ball.rotationDrag = 10;
+    
+    if (kb.pressed('left')) {
+        miniMe.changeAni('left');
+        miniMe.mirror.x = false;
+        miniMe.vel.x = -.5;
+        miniMe.vel.y = 0;
+    } else if (kb.pressed('up')){
+        miniMe.changeAni('back');
+        miniMe.vel.y = -.5;
+        miniMe.vel.x = 0;
+    } else if (kb.pressed('down')){
+        miniMe.changeAni('front');
+        miniMe.vel.y = .5;
+        miniMe.vel.x = 0;
+    } else if (kb.pressed('right')){
+        miniMe.changeAni('left');
+        miniMe.mirror.x = true;
+        miniMe.vel.y = 0;
+        miniMe.vel.x = .5;
     }
-
-    // make the spinning shape larger or smaller using the keyboard
-    // note the difference between presses (above) and pressing (here)
-    if (kb.pressing('right')) {
-        spinningShape.width += 10;
-    } else if (kb.pressing('left')) {
-        spinningShape.width -= 10;
-    }
-
-    // loop through the allSprites array and see how many are above the center of the screen
-    let currentScore = 0;
-    for (let sprite of allSprites) {
-        if (sprite.y < canvas.height/2) {
-            currentScore++;
-            if (currentScore > highScore) {
-                highScore = currentScore;
-            }
-        }
-    }
-
-    // display the score (minus 1 so it doesn't count the spinning shape)
-    text("BALLS: " + (currentScore-1), 40, 60);
-    text("HIGH:  " + (highScore-1), 40, 86);
-
 }

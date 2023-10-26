0407a1d26990c8e2314d669146fa8aeed33139fc
Alisha <144165734+aag19g@users.noreply.github.com>
2023-10-26 00:10:07 -0400

    Created a Tile BG. LEARN HOW TO RESIZE next

--- sketch.js
+++ sketch.js
@@ -1,51 +1,60 @@
 let miniMe;
-let tiles, tilesGroup;
+let sheetImg;
+let grass, dirt;
 
 function preload(){
-    miniMe = new Sprite(15, 14, 15, 14);
-    miniMe.spriteSheet = 'miniMe.png';
-    miniMe.anis.frameDelay = 8
-    miniMe.addAnis({
-        front: {row: 0, frames: 4},
-        back: {row: 1, frames: 4},
-        right: {row: 2, frames: 4},
-        left: {row: 3, frames: 4},
-    });
-    miniMe.changeAni('front');
-    miniMe.collider = "k"
+    sheetImg = loadImage("Textures-16.png");
+
 }
 
 function setup() {
-    new Canvas(500, 500, 'pixelated x4');
+    new Canvas(800, 800, 'pixelated x4');
     allSprites.pixelPerfect = true;
     allSprites.rotationLock = true;
 
-    tiles = new Group();
-    tiles.w = 10;
-    tiles.h = 10;
-    tiles.tile = '=';
-    tiles.collider = 's';
-    tiles.color = '#5E8C61'
+    grass = new Group();
+    grass.collider = 's';
+    grass.spriteSheet = sheetImg;
+    grass.addAni({w:16, h:16, row:4, col:3})
+    grass.tile = 'g';
+    grass.size(10, 10)
 
-    tilesGroup = new Tiles([
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
-        '================',
+    dirt = new Group();
+    dirt.collider = 's';
+    dirt.spriteSheet = sheetImg;
+    dirt.addAni({w:16, h:16, row:6, col:0})
+    dirt.tile = 'd';
+
+    new Tiles([
+        'gggggggggggggggg',
+        'gddddddgdddddddg',
+        'gggggdggggggggdg',
+        'ddddddddddddgddg',
+        'gggggggggggdgdgg',
+        'gddddgddddddgddg',
+        'gdgdddddggddgddg',
+        'gdgdgggdggdgggdg',
+        'gdggggggggdgdddg',
+        'gdgddddddgdgdggg',
+        'gdgdgggddgdgdddd',
+        'gdgdgggggggggdgg',
+        'gddddddddddddddg',
+        'gggggggggggggggg',
         ],
-        50,
-        40,
-        tiles.w + 0,
-        tiles.h + 0)
+        1, 2,
+        16, 16,);
+
+        miniMe = new Sprite(15, 14, 15, 14);
+        miniMe.spriteSheet = 'miniMe.png';
+        miniMe.anis.frameDelay = 7
+        miniMe.addAnis({
+            front: {row: 0, frames: 4},
+            back: {row: 1, frames: 4},
+            right: {row: 2, frames: 4},
+            left: {row: 3, frames: 4},
+        });
+        miniMe.changeAni('front');
+        miniMe.collider = "k"
 }
 
 function draw() { 
